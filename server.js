9fc77815fe7400302f1f8a6fb8a00cf6d95c649a
coolroushan <roushankumarcool@gmail.com>
2025-12-04 11:23:36 +0530

    delete-contact

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/visitors', require('./routes/visitorRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
-app.use('/api/contact', require('./routes/contactRoutes'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
